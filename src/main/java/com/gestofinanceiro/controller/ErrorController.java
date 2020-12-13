3e909dbaba823b76c7c7f54c9f6109feb8255712
Lucas <l.lima19@hotmail.com>
2020-12-13 05:29:08 -0300

    Controller para as paginas de erro 404 e 500

---
+++ src/main/java/com/gestofinanceiro/controller/ErrorController.java
@@ -0,0 +1,49 @@
+package com.gestofinanceiro.controller;
+
+import com.gestofinanceiro.model.Usuario;
+import com.gestofinanceiro.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request){
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        ModelAndView modelAndView = new ModelAndView();
+        Usuario usuarioLogado = usuarioService.usuarioLogado();
+
+        if (usuarioLogado != null) {
+            modelAndView.addObject("usuario", usuarioLogado);
+        }
+
+        if(status != null){
+            Integer statusCode = Integer.valueOf(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                modelAndView.setViewName("404");
+                return modelAndView;
+            }else if ( statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                modelAndView.setViewName("500");
+                return modelAndView;
+            }
+        }
+
+        modelAndView.setViewName("error");
+        return modelAndView;
+    }
+}
